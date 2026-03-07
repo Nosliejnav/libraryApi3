f2316fdd828a740c356fadb30798067d35882014
Nosliejnav <vanjeilsonm10@gmail.com>
2026-03-06 22:34:11 -0300

    Implementando as Specifications da entidade Livro

---
+++ src/main/java/com/vanja/libraryapi/repository/specs/LivroSpecs.java
@@ -0,0 +1,22 @@
+package com.vanja.libraryapi.repository.specs;
+
+import com.vanja.libraryapi.model.GeneroLivro;
+import com.vanja.libraryapi.model.Livro;
+import org.springframework.data.jpa.domain.Specification;
+
+public class LivroSpecs {
+
+    public static Specification<Livro> isbnEqual(String isbn){
+        return(root, query, cb) -> cb.equal(root.get("isbn"), isbn);
+    }
+
+    public static Specification<Livro> tituloLike(String titulo){
+        // upper(livro.titulo) like (%:param%)
+        return(root, query, cb) ->
+                cb.like(cb.upper(root.get("titulo")), "%" + titulo.toUpperCase() + "%");
+    }
+
+    public static Specification<Livro> generoEqual(GeneroLivro genero){
+         return (root, query, cb) -> cb.equal(root.get("genero"), genero);
+    }
+}
--- src/main/java/com/vanja/libraryapi/service/LivroService.java
+++ src/main/java/com/vanja/libraryapi/service/LivroService.java
@@ -3,6 +3,7 @@ package com.vanja.libraryapi.service;
 import com.vanja.libraryapi.model.GeneroLivro;
 import com.vanja.libraryapi.model.Livro;
 import com.vanja.libraryapi.repository.LivroRepository;
+import com.vanja.libraryapi.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 
 @Service
 @RequiredArgsConstructor
@@ -33,7 +35,38 @@ public class LivroService {
     public List<Livro> pesquisa(
             String isbn, String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicao){
 
-        Specification<Livro> specs = null;
+        // select * from livro where isbn = :isbn and nomeAutor =
+
+//        Specification<Livro> specs = Specification
+//                .where(LivroSpecs.isbnEqual(isbn))
+//                .and(LivroSpecs.tituloLike(titulo))
+//                .and(LivroSpecs.generoEqual(genero))
+//                ;
+
+        // select * from livro where 0 = 0
+
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if (isbn != null){
+            // query = query and isbn = :isbn
+            specs = specs.and(LivroSpecs.isbnEqual(isbn));
+        }
+
+        if (titulo != null){
+            specs = specs.and(LivroSpecs.tituloLike(titulo));
+        }
+
+        if(genero != null){
+            specs = specs.and(LivroSpecs.generoEqual(genero));
+        }
+
+//        Não mais usual depopis de criar o pacote e classe specs
+        //        // where isbn =:isbn
+//        Specification<Livro> isbnEqual = (root, query, cb ) ->
+//                cb.equal(root.get("isbn"), isbn);
+
+//        return repository.findAll(LivroSpecs.isbnEqual(isbn));
+
         return repository.findAll(specs);
     }
 }
